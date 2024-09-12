2be4d4174a771739c3a571cc418937a2218f7016
npersily1 <noah.persily@menloschool.org>
2024-09-11 19:05:49 -0700

    See worklog

--- src/HighwaysAndHospitals.java
+++ src/HighwaysAndHospitals.java
@@ -5,9 +5,8 @@ import java.util.ArrayList;
  * A puzzle created by Zach Blick
  * for Adventures in Algorithms
  * at Menlo School in Atherton, CA
- *
+ * <p>
  * Completed by: [YOUR NAME HERE]
- *
  */
 
 public class HighwaysAndHospitals {
@@ -18,22 +17,46 @@ public class HighwaysAndHospitals {
      */
     public static long cost(int n, int hospitalCost, int highwayCost, int cities[][]) {
 
-        if(highwayCost > hospitalCost) {
+        if (highwayCost > hospitalCost) {
             return n * hospitalCost;
         }
-        ArrayList<ArrayList<Integer>> subgraphs =new ArrayList<ArrayList<Integer>>();
+        ArrayList<ArrayList<Integer>> subgraphs = new ArrayList<ArrayList<Integer>>();
         // Seperate into subgraphs
         for (int i = 0; i < cities.length; i++) {
+            int temp1 = isInSubgraph(cities[i][0], subgraphs);
+            int temp2 = isInSubgraph(cities[i][1], subgraphs);
 
-        }
+            // If its a new subgraph
+            if ( temp1 < 0 && temp2 < 0) {
+                subgraphs.add(new ArrayList<Integer>());
+                subgraphs.get(subgraphs.size() - 1).add(cities[i][0]);
+                subgraphs.get(subgraphs.size() - 1).add(cities[i][1]);
+            }
+            // if temp1 is marked down
+            else if (temp1 >= 0 && temp2 < 0) {
+                subgraphs.get(temp1).add(cities[i][1]);
+            }
+            // if temp two is marked down
+            else if (temp2 >= 0 && temp1 < 0) {
+                subgraphs.get(temp2).add(cities[i][1]);
+            }
+        } int sum = 0;
         //  for each subgraph add the cost of hospital + (n-1) * highway cost to the total sum
+        for (int i = 0; i < subgraphs.size(); i++) {
+            sum += hospitalCost + ((subgraphs.get(i).size() - 1) * highwayCost);
+        }
 
 
+        return sum;
+    }
 
+    public static int isInSubgraph(int n, ArrayList<ArrayList<Integer>> subgraphs) {
 
-
-
-
-        return 0;
+        for (int i = 0; i < subgraphs.size(); i++) {
+            if (subgraphs.get(i).contains(n)) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
